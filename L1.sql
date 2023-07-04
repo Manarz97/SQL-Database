fb50c2efd8b50d1ed9d8dae7bb6fc3f886f18b2f
Manarz97 <138357870+Manarz97@users.noreply.github.com>
2023-07-04 12:42:22 +0400

    Update L1.sql

--- L1.sql
+++ L1.sql
@@ -1,10 +1,13 @@
+
+---Selecting All Columns
+
 select * from employees
 
 SELECT first_name,last_name
 FROM employees;
-
+--Selecting Specific Columns
 SELECT department_id, location_id FROM departments;
-
+--Using Arithmetic Operators
 SELECT last_name, salary, salary + 300
 FROM employees;
 
@@ -14,24 +17,32 @@ FROM employees;
 SELECT last_name, job_id, salary, commission_pct
 FROM employees;
 
+---Arithmetic expressions containing a null value evaluate to null.
+
 SELECT last_name, 12*salary*commission_pct
 FROM employees;
 
 SELECT last_name AS name, commission_pct comm
 FROM employees;
 
+---Using Column Aliases
+
 SELECT last_name "Name" , salary*12 "Annual Salary"
 FROM employees;
 
+--Concatenation Operator
 SELECT last_name||job_id AS "Employees"
 FROM employees;
 
+--Using Literal Character Strings
 SELECT last_name ||' is a '||job_id AS "Employee Details"
 FROM employees;
 
+--Alternative Quote (q) Operator
 SELECT department_name || q'[ Department's Manager Id: ]'|| manager_id 
 AS "Department and Manager" FROM departments;
 
+--Duplicate Rows.
 SELECT DISTINCT department_id
 FROM employees;
 
